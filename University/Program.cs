d534739631d7fc2d44a5f0cea14f9be9533bd4a6
Harut Safaryan <safaryanharut@gmail.com>
2021-11-05 07:58:27 +0400

    changes

--- University/Program.cs
+++ University/Program.cs
@@ -6,29 +6,27 @@ namespace University
     {
         static void Main(string[] args)
         {
+            Random rnd = new Random();
+            Teacher[] teachers = new Teacher[5];
+            Student[] students = new Student[34];
 
-            Teacher t1 = new Teacher("Jane", "english");
-            Teacher t2 = new Teacher("Jesika", "mathematics");
-            Teacher t3 = new Teacher("Nency", "physics");
+            for (int i = 0; i < teachers.Length; i++)
+            {
+                teachers[i] = new Teacher($"teacher_{i}", $"Course_{rnd.Next(1, 5)}");
+            }
 
-            Student s1 = new Student("s1", 10);
-            Student s2 = new Student("s2", 11);
-            Student s3 = new Student("s3", 12);
-            Student s4 = new Student("s4", 13);
-            Student s5 = new Student("s5", 14);
-            Student s6 = new Student("s6", 15);
-            Student s7 = new Student("s7", 16);
-
-            Student[] students = new Student[] { s1, s2, s3, s4, s5, s6, s7 };
-            Teacher[] teachers = new Teacher[] { t1, t2, t3 };
+            for (int i = 0; i < students.Length; i++)
+            {
+                students[i] = new Student($"student_{i}", rnd.Next(12, 20));
+            }
 
             University university = new University(students, teachers);
             Student s8 = new Student("s8", 17);
             university.Add(s8);
-            university.Remove(s3);
+            university.Remove(students[4]);
 
             university.DistribStudents();
-            var list = university.GetStudentsForteacher(t2);
+            var list = university.GetStudentsForteacher(teachers[4]);
 
             Console.WriteLine();
         }
